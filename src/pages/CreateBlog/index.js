1a57a4114c77fd62c4584975a9657159ed8218bb
sapit <syafiqshahrim19@gmail.com>
2021-02-19 06:09:50 +0800

    Implement Styling to create-blog page post

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {Input, Button, Upload, TextArea, Gap} from '../../components';
+import './createBlog.scss';
 
 const CreateBlog = () => {
     return (
-        <div>
-            <p>Content Create Blog Page</p>
+        <div className="blog-post">
+            <p className="title">Content Create Blog Page</p>
+            <Input label="Post Title"/>
+            <Upload />
+            <TextArea />
+            <Gap height={20}/>
+            <div className="button-action">
+            <Button title="Save"/>
+            </div>
         </div>
     )
 }
