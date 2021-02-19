2b06fe8c10294d17222feaf7c163b547e57568d8
sapit <syafiqshahrim19@gmail.com>
2021-02-19 09:56:26 +0800

    Implement styling for detail blog post

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { RegisterBg } from '../../assets';
+import './detailBlog.scss';
 
 const DetailBlog = () => {
     return (
-        <div>
-            <p>Content Detail Blog Page</p>
+        <div className="detail-blog-wrapper">
+            <img  className="img-cover" src={RegisterBg} alt="thumb"/>
+            <p className="blog-title">Title Blog</p>
+            <p className="blog-author">Author - Date Post</p>
+            <p className="blog-body">Adipisicing ut est in culpa occaecat qui non eu labore exercitation ea deserunt duis laboris.Reprehenderit qui ut magna mollit pariatur incididunt magna aute cillum excepteur.Culpa pariatur enim fugiat enim enim pariatur fugiat minim ad sunt pariatur in velit.</p>
         </div>
     )
 }
