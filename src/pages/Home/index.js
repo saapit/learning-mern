91b0d24cafd95dc555c236f3bbefc5de315f10df
sapit <syafiqshahrim19@gmail.com>
2021-02-23 09:52:37 +0800

    Using Axios to call API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BlogItem, Button, Gap } from '../../components'
 import './home.scss';
 import {useHistory} from 'react-router-dom';
+import axios from 'axios';
 
 const Home = () => {
+    useEffect(() => {
+        axios.get('http://localhost:4000/v1/blog/posts')
+        .then(result => {
+            console.log('data API', result.data);
+        })
+        .catch(err => {
+            console.log('error', err);
+        })
+    }, [])
     const history = useHistory();
     return (
         <div className="home-page-wrapper">
